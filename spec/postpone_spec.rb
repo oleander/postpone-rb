15db275026ce01be48798e09d81d910ad2d3bf04
Linus Oleander <linus@oleander.nu>
2012-01-26 20:16:32 +0100

    Adding #translate method in spec

--- spec/postpone_spec.rb
+++ spec/postpone_spec.rb
@@ -1,22 +1,23 @@
-describe Postpone do  
+
+describe Postpone::Invoke do  
   describe "instance" do
     subject { Example.new }
     it "should invoke stalker, without arguments" do
-      Stalker.should_receive(:enqueue).with("postpone.worker", {
-        args: Marshal.dump([]),
-        object: Marshal.dump(subject),
+      Stalker.should_receive(:enqueue).with("postpone.worker", translate({
+        args: [],
+        object: subject,
         method: :method1
-      })
+      }))
 
       subject.postpone.method1
     end
 
     it "should invoke stalker, with arguments" do
-      Stalker.should_receive(:enqueue).with("postpone.worker", {
-        args: Marshal.dump(["value1", "value2"]),
-        object: Marshal.dump(subject),
+      Stalker.should_receive(:enqueue).with("postpone.worker", translate({
+        args: ["value1", "value2"],
+        object: subject,
         method: :method2
-      })
+      }))
 
       subject.postpone.method2("value1", "value2")
     end
@@ -25,24 +26,31 @@ describe Postpone do
   describe "class method" do
     subject { Example }
     it "should invoke stalker, without arguments" do
-      Stalker.should_receive(:enqueue).with("postpone.worker", {
-        args: Marshal.dump([]),
-        object: Marshal.dump(subject),
+      Stalker.should_receive(:enqueue).with("postpone.worker", translate({
+        args: [],
+        object: subject,
         method: :method3
-      })
+      }))
 
       subject.postpone.method3
     end
 
     it "should invoke stalker, with arguments" do
-      Stalker.should_receive(:enqueue).with("postpone.worker", {
-        args: Marshal.dump(["value1", "value2"]),
-        object: Marshal.dump(subject),
+      Stalker.should_receive(:enqueue).with("postpone.worker", translate({
+        args: ["value1", "value2"],
+        object: subject,
         method: :method4
-      })
+      }))
 
       subject.postpone.method4("value1", "value2")
     end
-  end
-  
+  end  
 end
+
+# describe Postpone::Worker do
+#   describe "instance" do
+#     it "should call method" do
+#       Postpone::Worker.new(args)
+#     end
+#   end
+# end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,21 @@
 require "rspec"
 require_relative "../lib/postpone"
 
+module Helpers
+  def translate(hash, type = "symbol")
+    args = Marshal.dump(hash[:args])
+    object = Marshal.dump(hash[:object])
+    method = hash[:method]
+    
+    case type
+    when "symbol"
+      { args: args, object: object, method: method }
+    when "string"
+      { "args" => args, "object" => object, "method" => method }
+    end
+  end
+end
+
 class Example
   def method1; end
   def method2(value1, value2); end
@@ -10,4 +25,5 @@ end
 
 RSpec.configure do |config|
   config.mock_with :rspec
+  config.include Helpers
 end
